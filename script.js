f9866869911c68c6253c8901e3b6725cab1689ba
Sojemigolestov <165064992+Sojemigolestov@users.noreply.github.com>
2024-06-18 10:00:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function playaudio(){
+    let audio = document.getElementById("bark");
+    audio.play();
+    console.log("Bark right at you!");
+}
+
+//Add links to linkedin page
+//Add best way to contact me
+//Add profile photo
+//Add GitHub username
